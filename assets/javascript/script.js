1f1716a907b606639976f768f8f252279b5a8dab
Thomas Eubank <eubanktr@gmail.com>
2021-07-20 17:05:01 -0400

    weatherDashboard

---
+++ assets/javascript/script.js
@@ -0,0 +1,39 @@
+// var currentSearch = $('#searchTerm');
+// var searchButton = $('#searchBtn');
+var currentSearch = document.getElementById('searchTerm');
+var searchButton = document.getElementById('searchBtn');
+
+var recentSearches = $('#searchHistory');
+var currentWeatherCity = $('#currentCity');
+var cityForecast = $('#forecast');
+
+searchButton.onClick = searchSubHandler;
+
+
+function searchSubHandler(event) {
+    event.preventDefault();
+    let search = currentSearch.value.trim();
+    console.log(search);
+    //getWeather(search);
+};
+
+
+// function getWeather (search) {
+//     // const apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + '&appid=' + apiKey;
+//     const apiKey = "7e265aa18fe4cc94b7554c10023cca4a";
+
+//     const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${apiKey}`
+
+//     fetch(apiURL)
+//         .then(function (response) {
+//         if(response.ok) {
+//             response.json().then(function (data) {
+//                 console.log(data);
+//         });
+
+//         };
+    
+//     });
+// };
+
+
